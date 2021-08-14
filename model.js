c3112dfe4a017f2f845e337b39875d96542528f4
Sangwoong Yoon <sangwoong24yoon@gmail.com>
2021-08-15 00:35:54 +0900

    Upload pre-trained NAE and AE models; Enable NAE recon

--- model.js
+++ model.js
@@ -1,7 +1,21 @@
 async function loadModel() {
-  model = undefined;
-  model = await tf.loadGraphModel("./tfjs_AEmodel/model.json");
+  // conventional autoencoder
+  ae_encoder = undefined;
+  ae_decoder = undefined;
+  ae_encoder = await tf.loadLayersModel("./ae_encoder/model.json");
+  ae_decoder = await tf.loadLayersModel("./ae_decoder/model.json");
+
+  nae_encoder = undefined;
+  nae_decoder = undefined;
+  nae_encoder = await tf.loadLayersModel("./nae_encoder/model.json");
+  nae_decoder = await tf.loadLayersModel("./nae_decoder/model.json");
   console.log("model loaded");
+}
+
+function reconstruct(encoder, decoder, x) {
+  z = encoder.predict(x);
+  z = z.div(tf.norm(z, 2, 3, true));  // spherical projection
+  return decoder.predict(z);
 }
 
 function make_prediction() {
@@ -23,29 +37,32 @@ function make_prediction() {
   });
 
   const resized = tf.cast(input, "float32");
-  var t3d = tf.reshape(resized, [-1, 28, 28]);
+  var t3d = tf.reshape(resized, [-1, 28, 28, 1]);
   t3d = t3d.div(255);
-  const pred = model.predict(t3d);
+  // const pred = model.predict(t3d);
+  const ae_pred = reconstruct(ae_encoder, ae_decoder, t3d);
+  const nae_pred = reconstruct(nae_encoder, nae_decoder, t3d);
 
-  var a = pred;
+  var ae_values = ae_pred.mul(255).asType("int32").arraySync();
+  var nae_values = nae_pred.mul(255).asType("int32").arraySync();
   // map values from 0 -> 1 to 0 -> 255
-  a = a.mul(255);
-
-  var cast = a.asType("int32");
-  var values = cast.arraySync();
 
   var img = new Image();
   img.src = "./preset_image/empty.jpg";
 
   img.onload = function () {
-    var imageData = Inctx.getImageData(0, 0, 28, 28);
-    NAEctx.putImageData(testimg, 0, 0);
-    editPixels(imageData.data);
-    drawEditedImage(imageData);
-    reconError(testimg.data, imageData.data);
+    var AEImageData = Inctx.getImageData(0, 0, 28, 28);
+    var NAEImageData = Inctx.getImageData(0, 0, 28, 28);
+    // NAEctx.putImageData(testimg, 0, 0);
+    editPixels(AEImageData.data, ae_values);
+    editPixels(NAEImageData.data, nae_values);
+    drawEditedImage(AEctx, AEImageData);
+    drawEditedImage(NAEctx, NAEImageData);
+    reconError("AErecon", testimg.data, AEImageData.data);
+    reconError("NAErecon", testimg.data, NAEImageData.data);
   };
 
-  function reconError(beforeData, afterData) {
+  function reconError(elementid, beforeData, afterData) {
     var tf_result = 0;
     //tf_result = tf.squaredDifference(Array.from(beforeData), Array.from(afterData));
     tf_result = tf.losses.meanSquaredError(
@@ -55,10 +72,10 @@ function make_prediction() {
     tf_result = tf_result.arraySync();
     tf_result = tf_result / 100;
     tf_result = tf_result.toFixed(3);
-    document.getElementById("AErecon").innerHTML = tf_result;
+    document.getElementById(elementid).innerHTML = tf_result;
   }
 
-  function editPixels(imgData) {
+  function editPixels(imgData, values) {
     var x = 0;
     var y = 0;
 
@@ -76,7 +93,7 @@ function make_prediction() {
     }
   }
 
-  function drawEditedImage(newData) {
-    AEctx.putImageData(newData, 0, 0);
+  function drawEditedImage(ctx, newData) {
+    ctx.putImageData(newData, 0, 0);
   }
 }
